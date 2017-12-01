99f3340e2182e300ec3b2798c432f79fa20366c9
mramos <max_a_ramos@yahoo.com>
2017-12-01 16:25:00 +0800

    Added collection test

---
+++ src/test/java/ph/mramos/ocjp8/collections/CollectionTest.java
@@ -0,0 +1,16 @@
+package ph.mramos.ocjp8.collections;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class CollectionTest {
+
+	@Test
+	public void testList() {
+		String[] array = {"a", "b"};
+		List<String> list = Arrays.asList(array);
+	}
+	
+}
