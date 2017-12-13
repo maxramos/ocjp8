b810e442f0b58140bbb8b82aae3225c9940f5299
mramos <max_a_ramos@yahoo.com>
2017-12-14 00:27:54 +0800

    Updated collector and comparator tests

--- src/test/java/ph/mramos/ocjp8/comparator/ComparatorTest.java
+++ src/test/java/ph/mramos/ocjp8/comparator/ComparatorTest.java
@@ -9,15 +9,28 @@ import org.junit.Test;
 public class ComparatorTest {
 
 	@Test
-	public void testComparator() {
+	public void testComparator1() {
 		List<Car> cars = new ArrayList<>();
 		cars.add(new Car("Toyota", "Vios"));
 		cars.add(new Car("Toyota", "Corolla"));
 		cars.add(new Car("Hyundai", "Accent"));
 		cars.add(new Car("Hyundai", "Elantra"));
 				
-		cars.sort((c1, c2) -> Comparator.comparing((Car c) -> c.getBrand()).thenComparing(c -> c.getModel()).compare(c1, c2));
+		Comparator<Car> comparator = Comparator.comparing(Car::getBrand).thenComparing(Car::getModel);
+		cars.sort(comparator);
+		System.out.println(cars);
+	}
+	
+	@Test
+	public void testComparator2() {
+		List<Car> cars = new ArrayList<>();
+		cars.add(new Car("Toyota", "Vios"));
+		cars.add(new Car("Toyota", "Corolla"));
+		cars.add(new Car("Hyundai", "Accent"));
+		cars.add(new Car("Hyundai", "Elantra"));
 		
+		Comparator<Car> comparator = Comparator.comparing(Car::getBrand, (x, y) -> x.length() - y.length());
+		cars.sort(comparator);
 		System.out.println(cars);
 	}
 	
