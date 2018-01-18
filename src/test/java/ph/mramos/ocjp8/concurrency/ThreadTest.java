99fc1a7f0a62dea6667784185985978d9ccac44f
mramos <max_a_ramos@yahoo.com>
2018-01-19 02:58:00 +0800

    Added executor service test

--- src/test/java/ph/mramos/ocjp8/concurrency/ThreadTest.java
+++ src/test/java/ph/mramos/ocjp8/concurrency/ThreadTest.java
@@ -1,5 +1,8 @@
 package ph.mramos.ocjp8.concurrency;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.junit.Test;
 
 public class ThreadTest {
@@ -22,6 +25,42 @@ public class ThreadTest {
 		for (int i = 0; i < 100; i++) {	
 			System.out.println("End");
 		}
+	}
+	
+	@Test
+	public void testExecute() {
+		Runnable runnable1 = () -> {
+			for (int i = 0; i < 100; i++) {	
+				System.out.println("Test1");
+			}
+		};
+		
+		Runnable runnable2 = () -> {
+			for (int i = 0; i < 100; i++) {	
+				System.out.println("Test2");
+			}
+		};
+		
+		Runnable runnable3 = () -> {
+			for (int i = 0; i < 100; i++) {	
+				System.out.println("Test3");
+			}
+		};
+		
+		ExecutorService es = Executors.newSingleThreadExecutor();
+		es.execute(runnable1);
+		es.execute(runnable2);
+		es.execute(runnable3);
+		
+		for (int i = 0; i < 100; i++) {	
+			System.out.println("Start");
+		}
+		
+		for (int i = 0; i < 100; i++) {	
+			System.out.println("End");
+		}
+		
+		es.shutdown();
 	}
 	
 }
